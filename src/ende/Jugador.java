0bb542301d0f3eba751d89ece32850ddb05eefd0
mcventur <marcelinopenide@gmail.com>
2022-05-04 10:38:54 +0200

    Modificado el switch case por el switch mejorado de java 12: https://docs.oracle.com/en/java/javase/13/language/switch-expressions.html

--- src/ende/Jugador.java
+++ src/ende/Jugador.java
@@ -12,14 +12,9 @@ class Jugador {
         String opcion = "";
         Integer c = (int) (Math.random() * 3);
         switch (c) {
-            case 0:
-                opcion = ("piedra");
-                break;
-            case 1:
-                opcion = ("papel");
-                break;
-            case 2:
-                opcion = ("tijeras");
+            case 0 -> opcion = ("piedra");
+            case 1 -> opcion = ("papel");
+            case 2 -> opcion = ("tijeras");
         }
         return opcion;
     }
